484625c963ec82372939fb5ecf348c4eb70bd5e3
Piotr Juszczak <piort789@gmail.com>
2024-02-05 02:50:35 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <RecipePage />
       <div className="attribution">
-        Challenge by
+        {"Challenge by "}
         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
           Frontend Mentor
         </a>
--- src/Components/RecipePage/RecipePage.jsx
+++ src/Components/RecipePage/RecipePage.jsx
@@ -23,85 +23,134 @@ export default function RecipePage() {
             <h3 className="preperation-time-header">Preparation time</h3>
 
             <ul className="preperation-time-list">
-              <li className="preperation-time-list-item">
+              <li className="preperation-time-item">
                 <span>
                   <strong>Total: </strong>Approximately 10 minutes
                 </span>
               </li>
 
-              <li className="preperation-time-list-item">
-                <strong>Preparation: </strong>5 minutes
+              <li className="preperation-time-item">
+                <span>
+                  <strong>Preparation: </strong>5 minutes
+                </span>
               </li>
 
-              <li className="preperation-time-list-item">
-                <strong>Cooking: </strong>5 minutes
+              <li className="preperation-time-item">
+                <span>
+                  <strong>Cooking: </strong>5 minutes
+                </span>
               </li>
             </ul>
           </section>
 
           <section className="ingredients-container">
             <h2 className="section-title">Ingredients</h2>
-            <ol className="ingredient-list">
-              <li className="ingredient">2-3 large eggs</li>
-              <li className="ingredient">Salt, to taste</li>
-              <li className="ingredient">Pepper, to taste</li>
-              <li className="ingredient"> 1 tablespoon of butter or oil</li>
+            <ul className="ingredient-list">
               <li className="ingredient">
-                Optional fillings: cheese, diced vegetables, cooked meats, herbs
+                <span>2-3 large eggs</span>
               </li>
-            </ol>
+              <li className="ingredient">
+                <span>Salt, to taste</span>
+              </li>
+              <li className="ingredient">
+                <span>Pepper, to taste</span>
+              </li>
+              <li className="ingredient">
+                {" "}
+                <span>1 tablespoon of butter or oil</span>
+              </li>
+              <li className="ingredient">
+                <span>
+                  Optional fillings: cheese, diced vegetables, cooked meats,
+                  herbs
+                </span>
+              </li>
+            </ul>
           </section>
 
-          <hr className="section-septarator" />
+          <hr className="section-separator" />
 
           <section className="instructions-container">
             <h2 className="section-title">Instructions</h2>
             <ol className="instruction-list">
               <li className="instruction-step">
-                <strong>Beat the eggs:</strong>
-                In a bowl, beat the eggs with a pinch of salt and pepper until
-                they are well mixed. You can add a tablespoon of water or milk
-                for a fluffier texture.
+                <span>
+                  <strong>Beat the eggs:</strong>
+                  In a bowl, beat the eggs with a pinch of salt and pepper until
+                  they are well mixed. You can add a tablespoon of water or milk
+                  for a fluffier texture.
+                </span>
               </li>
 
               <li className="instruction-step">
-                <strong>Heat the pan:</strong>
-                Place a non-stick frying pan over medium heat and add butter or
-                oil.
+                <span>
+                  <strong>Heat the pan:</strong>
+                  Place a non-stick frying pan over medium heat and add butter
+                  or oil.
+                </span>
               </li>
 
               <li className="instruction-step">
-                <strong>Cook the omelette:</strong>
-                Once the butter is melted and bubbling, pour in the eggs. Tilt
-                the pan to ensure the eggs evenly coat the surface.
+                <span>
+                  <strong>Cook the omelette:</strong>
+                  Once the butter is melted and bubbling, pour in the eggs. Tilt
+                  the pan to ensure the eggs evenly coat the surface.
+                </span>
               </li>
 
               <li className="instruction-step">
-                <strong>Add fillings (optional):</strong>
-                When the eggs begin to set at the edges but are still slightly
-                runny in the middle, sprinkle your chosen fillings over one half
-                of the omelette.
+                <span>
+                  <strong>Add fillings (optional):</strong>
+                  When the eggs begin to set at the edges but are still slightly
+                  runny in the middle, sprinkle your chosen fillings over one
+                  half of the omelette.
+                </span>
               </li>
 
               <li className="instruction-step">
-                <strong>Fold and serve:</strong>
-                As the omelette continues to cook, carefully lift one edge and
-                fold it over the fillings. Let it cook for another minute, then
-                slide it onto a plate.
+                <span>
+                  <strong>Fold and serve:</strong>
+                  As the omelette continues to cook, carefully lift one edge and
+                  fold it over the fillings. Let it cook for another minute,
+                  then slide it onto a plate.
+                </span>
               </li>
 
               <li className="instruction-step">
-                <strong>Enjoy:</strong>
-                Serve hot, with additional salt and pepper if needed.
+                <span>
+                  <strong>Enjoy:</strong>
+                  Serve hot, with additional salt and pepper if needed.
+                </span>
               </li>
             </ol>
           </section>
 
-          <hr className="section-septarator" />
+          <hr className="section-separator" />
 
           <section className="nutrition-container">
             <h2 className="section-title">Nutrition</h2>
-            <p>Calories 277kcal Carbs 0g Protein 20g Fat 22g</p>
+            <p className="nutrition-description">
+              The table below shows nutritional values per serving without the
+              additional fillings
+            </p>
+            <table className="nutrition-table">
+              <tr className="nutrition-table-row">
+                <th className="nutrition-category">Calories</th>
+                <td className="nutrition-data">277kcal </td>
+              </tr>
+              <tr className="nutrition-table-row">
+                <th className="nutrition-category">Carbs</th>
+                <td className="nutrition-data">0g </td>
+              </tr>
+              <tr className="nutrition-table-row">
+                <th className="nutrition-category">Protein</th>
+                <td className="nutrition-data">20g </td>
+              </tr>
+              <tr className="nutrition-table-row">
+                <th className="nutrition-category">Fat</th>
+                <td className="nutrition-data">22g </td>
+              </tr>
+            </table>
           </section>
         </div>
       </main>
